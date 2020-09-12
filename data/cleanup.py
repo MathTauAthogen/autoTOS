9a0a40ee446ca7706bc462ed43a55c1eab0a2a6f
William Qin <wqin2008@gmail.com>
2020-09-12 01:33:54 -0400

    And iterate over all sites

--- data/cleanup.py
+++ data/cleanup.py
@@ -26,40 +26,45 @@ def removerange(a, b, lens, labels, (sub,sub2)):
 
 ###############################################################################################################
 
-b = raw_input("Which TOS?")#TEMP: Test on one TOS at a time
-
 #Initialize the Labelled Excerpts Data Table
 
 df = pd.read_csv (r'labeled_excerpts.csv.bak')
-sitedf = df[df['slug'] == b]
+
+c = []
+
+sluglist = df.slug.unique()
+
+print(sluglist)
 
 ###############################################################################################################
+for b in sluglist:
+    sitedf = df[df['slug'] == b]
+    full = "" # The full text of the TOS of the site
 
-tokenlist = {} # A list of all of the sentences, which are newline-separated in the input.
-full = "" # The full text of the TOS of the site
+    with open("tos/"+b+".txt", "r") as g:
+        full = g.read() #Get the full text
+        printable = set(string.printable)
+        full = filter(lambda x: x in printable, full)
+        tokenlistb = full.splitlines() #Get a list of sentences
+        lens = [sum(map(len,tokenlistb[:i]))+i+1 for i in range(len(tokenlistb) + 1)] #Find the starting index of each sentences
 
-with open("tos/"+b+".txt", "r") as g:
-    full = g.read() #Get the full text
-    printable = set(string.printable)
-    full = filter(lambda x: x in printable, full)
-    tokenlistb = full.splitlines() #Get a list of sentences
-    lens = [sum(map(len,tokenlistb[:i]))+i+1 for i in range(len(tokenlistb) + 1)] #Find the starting index of each sentences
+        full = " ".join(full.split('\n')) #Put the full text on only one line.
 
-    full = " ".join(full.split('\n')) #Put the full text on only one line.
+    labels = [('','','','') for i in range(len(tokenlistb))] #Initialize something containing inner tuples that can be indexed to the same amount as the other values
 
-labels = [('','','','') for i in range(len(tokenlistb))] #Initialize something containing inner tuples that can be indexed to the same amount as the other values
+    #sub = label, iterate over all
+    #sub2 = its class_id
 
-#sub = label, iterate over all
-#sub2 = its class_id
+    for i, row in sitedf.iterrows():
+        sub = " ".join(row['excerpt'].split('\n'))
+        sub2 = row['class_id']
+        ind = full.index(sub)
+        lens, labels = removerange(ind, ind + len(sub), lens, labels, (sub, sub2))
 
-for i, row in sitedf.iterrows():
-    sub = " ".join(row['excerpt'].split('\n'))
-    sub2 = row['class_id']
-    ind = full.index(sub)
-    lens, labels = removerange(ind, ind + len(sub), lens, labels, (sub, sub2))
+    #And finish it off
 
-#And finish it off
-
-c = [{"token":full[lens[i]-1:lens[i+1] - 2], "labels":[{"text":labels[i][0], "start":labels[i][2]-lens[i]+2, "end":labels[i][3]-lens[i]+1, "class_id":labels[i][1]} for j in range(i,i+1) if labels[j] != ('','','','')]} for i in range(len(labels))]
+    dff = [{"token":full[lens[i]-1:lens[i+1] - 2], "labels":[{"text":labels[i][0], "start":labels[i][2]-lens[i]+2, "end":labels[i][3]-lens[i]+1, "class_id":labels[i][1]} for j in range(i,i+1) if labels[j] != ('','','','')]} for i in range(len(labels))]
+    c += dff
+    print(json.dumps(dff))
 
 print(json.dumps(c))
