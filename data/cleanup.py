b252bf95cf5974e75ae6f69df3acee744b396cc5
William Qin <wqin2008@gmail.com>
2020-09-12 01:08:40 -0400

    New and improved cleanup

--- data/cleanup.py
+++ data/cleanup.py
@@ -4,10 +4,11 @@
 #
 # TODO list:
 # 1. Loop over all sites
-# 2. Introduce some kind of code standards
-# 3. Test with real data
+# 2. Test with real data
 
 import pandas as pd
+import json
+import string
 
 # Function removerange
 # Inputs: a, the first index of the substring; b, the last index of the substring; lens, an array of the current indices of the tokens; labels, a current list of all of the info about the tokens.
@@ -19,7 +20,7 @@ def removerange(a, b, lens, labels, (sub,sub2)):
         if(inb and lens[i]>a):
             inb = False
             c += [i]
-        elif((not inb) and lens[i]>b):
+        if((not inb) and lens[i]>b):
             c += [i]
             break
     return [lens[:c[0]]+lens[c[1]:], labels[:c[0] - 1]+[(sub,sub2,a,b)]+labels[c[1]:]]
@@ -38,12 +39,14 @@ sitedf = df[df['slug'] == b]
 tokenlist = {} # A list of all of the sentences, which are newline-separated in the input.
 full = "" # The full text of the TOS of the site
 
-with open(b+".txt", "r") as g:
+with open("tos/"+b+".txt", "r") as g:
     full = g.read() #Get the full text
+    printable = set(string.printable)
+    full = filter(lambda x: x in printable, full)
     tokenlistb = full.splitlines() #Get a list of sentences
-    lens = [sum(map(len,tokenlistb[:i])) for i in range(len(tokenlistb) + 1)] #Find the starting index of each sentences
+    lens = [sum(map(len,tokenlistb[:i]))+i+1 for i in range(len(tokenlistb) + 1)] #Find the starting index of each sentences
 
-    full = "".join(full.split('\n')) #Put the full text on only one line.
+    full = " ".join(full.split('\n')) #Put the full text on only one line.
 
 labels = [('','','','') for i in range(len(tokenlistb))] #Initialize something containing inner tuples that can be indexed to the same amount as the other values
 
@@ -58,6 +61,6 @@ for i, row in sitedf.iterrows():
 
 #And finish it off
 
-c = [{"token":full[lens[i]:lens[i+1] - 1], "labels":[{"text":labels[i][0], "start":labels[i][2], "end":labels[i][3], "class_id":labels[i][1]}]} for i in range(len(labels)) if labels[i] != ('','','','')]
+c = [{"token":full[lens[i]-1:lens[i+1] - 2], "labels":[{"text":labels[i][0], "start":labels[i][2]-lens[i]+2, "end":labels[i][3]-lens[i]+1, "class_id":labels[i][1]} for j in range(i,i+1) if labels[j] != ('','','','')]} for i in range(len(labels))]
 
-print(c)
+print(json.dumps(c))
