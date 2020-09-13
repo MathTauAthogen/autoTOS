f87dec5ef3c9fb8a4fe5a43a51d76cae1f1b9eca
Andrew Mascillaro <amascillaro@gmail.com>
2020-09-13 02:13:02 -0400

    Add setup.py for cloud deployment

---
+++ prediction/setup.py
@@ -0,0 +1,13 @@
+from distutils.command.sdist import sdist as sdist_orig
+from distutils.errors import DistutilsExecError
+
+from setuptools import setup, os
+
+os.system('sh install_finetune_and_spacy.sh')
+
+setup(
+    name='auto_tos_summarizer',
+    version='0.1',
+    scripts=['predictor.py'],
+    install_requires=['tensorflow==2.2.0', 'tensorflow-addons==0.10.0',
+    	"tensorflow-gpu"])
