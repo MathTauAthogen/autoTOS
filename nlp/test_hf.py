7b2afa4925122677153056cbe1818fc538b1f968
Andrew Mascillaro <amascillaro@gmail.com>
2020-10-18 23:55:20 -0400

    Add modified predictor and test scripts for the huggingface model

---
+++ nlp/test_hf.py
@@ -0,0 +1,32 @@
+from finetune.util.metrics import annotation_report
+from sklearn.metrics import classification_report
+
+from transformers import TFRobertaForSequenceClassification
+import json
+import sys
+from train_hf import convert_model_data
+
+
+def evaluate(tokens):
+    model = TFRobertaForSequenceClassification.load("checkpoints/model.ckpt", from_tf=True)
+    return model.predict(tokens)
+
+
+def test(test_data):
+    tokens, labels = convert_model_data(test_data)
+
+    predictions = evaluate(tokens)
+    print(predictions)
+    print(classification_report(labels, predictions))
+
+    return predictions
+
+
+if __name__ == "__main__":
+
+    with open(sys.argv[1], "r") as f:
+        test_set = json.loads(f.read())
+
+    output = test(test_set)
+    with open("outputs/predictions.json", "w+") as out:
+        out.write(json.dumps(output))
